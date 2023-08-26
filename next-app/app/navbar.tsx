f8aa0519cbb0e91c9d90b49fcfaa5b1cdcb503ce
Sophie <szaniszlozs7@gmail.com>
2023-08-26 20:15:20 +0200

    add navbar, footer with beta design

---
+++ next-app/app/navbar.tsx
@@ -0,0 +1,9 @@
+export default function NavBar() {
+  return (
+    <div className="fixed w-screen h-16 bg-gradient-to-b from-white via-white dark:from-gray-800 dark:via-gray-800">
+      <div className="text-right pt-1 pr-5 text-white">
+        <p>Login</p>
+      </div>
+    </div>
+  );
+}
