4015fc6641b90ea80474ab2f9ca4e8935a1df062
Sophie <szaniszlozs7@gmail.com>
2023-08-30 23:36:48 +0200

    hungarian version of login

--- next-app/app/navbar.tsx
+++ next-app/app/navbar.tsx
@@ -2,7 +2,7 @@ export default function NavBar() {
   return (
     <div className="fixed w-screen h-16 bg-gradient-to-b from-white via-white dark:from-gray-800 dark:via-gray-800">
       <div className="text-right pt-1 pr-5 text-white">
-        <p>Login</p>
+        <p>Bejelentkezés</p>
       </div>
     </div>
   );
