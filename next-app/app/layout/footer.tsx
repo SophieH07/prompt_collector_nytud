397133b076c824fe5366c838c4955adf53759a97
Sophie <szaniszlozs7@gmail.com>
2023-08-31 23:27:50 +0200

    footer design fix

--- next-app/app/layout/footer.tsx
+++ next-app/app/layout/footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <div className="fixed bottom-0 flex h-16 justify-center w-full bg-gradient-to-t from-white via-white dark:from-gray-800 dark:via-gray-800 text-white">
+    <div className="fixed bottom-0 flex h-16 justify-center w-full bg-gradient-to-t from-gray-800 via-gray-800 text-white">
       <p className="pt-7">Nyelvtudományi Intézet</p>
     </div>
   );
