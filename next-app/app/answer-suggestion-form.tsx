bed897659d36c6ddfc019b7a6580eecb0186aef5
Sophie <szaniszlozs7@gmail.com>
2024-01-10 10:17:11 +0100

    add showID from all

--- next-app/app/answer-suggestion-form.tsx
+++ next-app/app/answer-suggestion-form.tsx
@@ -1,7 +1,7 @@
 "use client";
 /* eslint no-use-before-define: 0 */
 import { useState, FormEvent, useEffect } from "react";
-import { useRouter } from 'next/navigation'
+import { useRouter } from "next/navigation";
 
 export default function AnswerSuggestionForm() {
   let [prompt, setPrompt] = useState("");
@@ -15,35 +15,34 @@ export default function AnswerSuggestionForm() {
   const router = useRouter();
   async function fetchPrompt() {
     try {
-      const response = await fetch("/api/answer-suggest");  
+      const response = await fetch("/api/answer-suggest");
       if (response.status === 200) {
         const data = await response.json();
         setFetchedPrompt(data);
         setPrompt(data.prompt);
-        setAnswer(data.response || '');
+        setAnswer(data.response || "");
       } else {
-        console.error('Failed to fetch prompt');
+        console.error("Failed to fetch prompt");
       }
     } catch (error) {
-      console.error('Error fetching prompt:', error);
+      console.error("Error fetching prompt:", error);
     }
   }
   useEffect(() => {
-
     fetchPrompt();
   }, []);
 
   const restoreForm = () => {
     setPrompt(fetchedPrompt.prompt);
-    setAnswer(fetchedPrompt.response || '');
-    setSuggestion('');
-    setStatus('');
-  }
+    setAnswer(fetchedPrompt.response || "");
+    setSuggestion("");
+    setStatus("");
+  };
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     console.log(event.target.value);
     setStatus(event.target.value);
-};
+  };
 
   async function onSubmit(event: FormEvent<HTMLFormElement>) {
     event.preventDefault();
@@ -59,7 +58,7 @@ export default function AnswerSuggestionForm() {
           body: JSON.stringify({
             id: fetchedPrompt.id,
             suggestion: answer,
-            status: status || 'UNGRADED',
+            status: status || "UNGRADED",
           }),
         });
 
@@ -103,27 +102,57 @@ export default function AnswerSuggestionForm() {
             </div>
             <div className="pt-5">
               <label className="block uppercase tracking-wide text-gray-700 text-xl font-bold mb-2">
-                Válasz
+                Válasz {fetchedPrompt?.id - 5000}/5000
               </label>
               <textarea
                 className="block resize p-6 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500"
                 readOnly
-                value={fetchedPrompt?.response || ''}
+                value={fetchedPrompt?.response || ""}
               />
             </div>
             <div className="flex space-x-4">
-              <input type="radio" id="bad" name="status" value="BAD" onChange={handleChange} />
+              <input
+                type="radio"
+                id="bad"
+                name="status"
+                value="BAD"
+                onChange={handleChange}
+              />
               <label htmlFor="bad">Rossz</label>
-              <input type="radio" id="faulty" name="status" value="FAULTY" onChange={handleChange} />
+              <input
+                type="radio"
+                id="faulty"
+                name="status"
+                value="FAULTY"
+                onChange={handleChange}
+              />
               <label htmlFor="faulty">Hibás</label>
-              <input type="radio" id="medium" name="status" value="MEDIUM" onChange={handleChange} />
+              <input
+                type="radio"
+                id="medium"
+                name="status"
+                value="MEDIUM"
+                onChange={handleChange}
+              />
               <label htmlFor="medium">Közepes</label>
-              <input type="radio" id="good" name="status" value="GOOD" onChange={handleChange} />
+              <input
+                type="radio"
+                id="good"
+                name="status"
+                value="GOOD"
+                onChange={handleChange}
+              />
               <label htmlFor="good">Rendben van</label>
-              <input type="radio" id="perfect" name="status" value="PERFECT" onChange={handleChange} />
+              <input
+                type="radio"
+                id="perfect"
+                name="status"
+                value="PERFECT"
+                onChange={handleChange}
+              />
               <label htmlFor="perfect">Tökéletes</label>
             </div>
-            
+
             <div className="pt-5">
               <label className="block uppercase tracking-wide text-gray-700 text-xl font-bold mb-2">
                 Válasz javítás
@@ -132,10 +161,8 @@ export default function AnswerSuggestionForm() {
                 className="block resize p-6 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500"
                 onChange={(e) => setAnswer(e.target.value)}
                 value={answer}
-                
               />
             </div>
-            
           </div>
           <div className="justify-center flex space-x-16 pt-4">
             <input
@@ -143,13 +170,13 @@ export default function AnswerSuggestionForm() {
               value="Kihagyás"
               className="text-white bg-red-700 hover:bg-red-800 cursor-pointer focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm w-full lg:py-3 sm:w-auto px-5 py-2.5 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800"
               onClick={() => restoreForm()}
-              />
+            />
             <input
               type="button"
               value="Újra"
               className="text-white bg-green-500 hover:bg-green-600 cursor-pointer focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm w-full lg:py-3 sm:w-auto px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
               onClick={() => restoreForm()}
-              />
+            />
             <input
               type="submit"
               value="Mehet"
